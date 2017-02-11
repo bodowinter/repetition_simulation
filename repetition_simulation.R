70ed8c22353ac5308c7ac2417a129bd1401976d9
Bodo Winter <winterb@englis-2lffvh6.home>
2017-02-11 17:33:54 +0000

    first commit for setting up files and repository

---
+++ repetition_simulation.R
@@ -0,0 +1,12 @@
+## Bodo Winter & Timo Roettger
+## Type I/II error simulations for repetitions
+## Created Feb 11, 2017
+
+## Design and data is inspired by Roettger et al. (2014)'s paper
+## on incomplete neutralization
+
+##------------------------------------------------------------------
+## Preliminaries
+##------------------------------------------------------------------
+
+setwd('/Users/winterb/Research/repetitions/simulations/')
