efd350bcd0f5e74aaf1755d3bf19688a9e75fcc9
Bodo Winter <winterb@englis-2lffvh6.home>
2017-02-11 17:41:18 +0000

    parameters

--- repetition_simulation.R
+++ repetition_simulation.R
@@ -10,3 +10,22 @@
 ##------------------------------------------------------------------
 
 setwd('/Users/winterb/Research/repetitions/simulations/')
+
+## Initialize variability values taken from the model of Experiment 1:
+
+it_int_sd <- 27.210
+it_slope_sd <- 1.931
+sub_int_sd <- 30.636
+sub_slope_sd <- 5.786
+res_sd <- 19.863
+IN_effect <- -8.655
+
+## Initialize parameters (N. of sub, item and repetition):
+
+n_reps <- 2 ^ (0:4)
+n_its <- 2 ^ (0:5)
+n_subs <- 6 * 2 ^ (0:3)
+
+
+
+
